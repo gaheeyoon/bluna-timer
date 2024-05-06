3113f772c2c0028a81dfa7b6b9099b1e9ee08dcb
gaheeyoon <a5312559@gmail.com>
2024-05-06 10:46:54 +0900

    랜덤한 순서로 재생되게 수정

--- src/js/clock.js
+++ src/js/clock.js
@@ -25,41 +25,35 @@ let isStart = false;
 let isFirstStart = true;
 let interval;
 
-let videoUrlList = ['yRTnfhynaRc', 'igXk6QiM7k0', 'oGortIMXvR4', 'exJILuVWKqY', 'JyvUjMbT-Qc',
-    'UEDunnBrmOg', 'wJsifVTzQuc', 'u-p1umePZ-o', 'Wxc7XqUQBNQ', 'r8kVtq1dAik',
-    '0_hO0sp5Gxc', 'hmBBZUd-eDk', 'OVR5SO0DvxE', '15wrFG1Gz84', 'dYUiGU3v8ew',
-    'J37gHwn9904', '-cFpgpv4G0Y', '38opfjfWlgY', 'aWDpwkKktaU', 'A1leiL8wknU',
-    'yhewMcvCyTU', 'RjzSPIdPXl0', '9aZmlntPYk4', '9MPjcB4n-Vc', '1AFW54NnDtQ',
-    'FHdmgF7h8Wk', 'GLxYjLkR1ts', '5zsYEfujI24', 'P0q29xLexXc', 'pxCDtROfuFs',
-    '8HZ698TcLtU', 'LlRX9FdUonI', '8BirxRSxOHk', 'xzCw7lM4Cls', 'TiURA4Ch1oU',
-    'DSI6GXpkru0', '110JyIgQvOI', 'Anyo9iUBjRk', 'Y7BS3wZTVvM', 'fxAYwvRjrjs',
-    '6d65eRnhrAw', 'AKfU9hnhck4', 'Dqf36LmjQHw', 'SABakcXwFik', 'nZlfIGiwPZ8',
-    'mt9VSxZ1PKY', 'lHXJRx9Dq4Y', 'SWQlsfE6OpQ', '0cwmFM0dEgw', 'QB-sCoQkhfQ',
-    'FmZsX1f1esc'
-]
-let videoNameList = ['April', 'Falling petals', 'Swimming', 'Shape', 'Zero gravity',
-    'Someone', 'With cactus', 'Oasis', 'In your time that flows differently than I do', 'Indigo blossom',
-    'A long day', 'Deep nap', 'Coming to you', 'Day after day', 'In my dreams',
-    'How are you', 'Hope', 'Crossroads', 'Return', 'Name of life',
-    'When the morning comes', 'Flowing flower petal', 'Spring night', 'Cherry blossom dance', 'Magnolia',
-    'Spring in winter', 'Snow and stars', 'Mini bell', 'Deep blue of dawn', 'Patting',
-    'A moment with you', 'Small room and old piano', 'Whispering', 'Hug in the moonlight', 'Approach',
-    'May 26, Memories', 'May 29, Love in me', 'Moon', 'Wind', 'A letter from the moon',
-    'Firefly', 'Violet', 'Death on life', 'Snow Blossom', 'The deep blue night',
-    'Silhouette of sunlight', 'Evening primrose', 'Butterfly dance', 'Silent tremor', 'Wavy',
-    'Winter blue'
-]
+let videoUrlList = [ {title: 'April', url: 'yRTnfhynaRc'}, {title: 'Falling petals', url: 'igXk6QiM7k0'}, {title: 'Swimming', url: 'oGortIMXvR4'}, 
+    {title: 'Shape', url: 'exJILuVWKqY'}, {title: 'Zero gravity', url: 'JyvUjMbT-Qc'}, {title: 'Someone', url: 'UEDunnBrmOg'}, 
+    {title: 'With cactus', url: 'wJsifVTzQuc'}, {title: 'Oasis', url: 'u-p1umePZ-o'}, {title: 'In your time that flows differently than I do', url: 'Wxc7XqUQBNQ'}, 
+    {title: 'Indigo blossom', url: 'r8kVtq1dAik'}, {title: 'A long day', url: '0_hO0sp5Gxc'}, {title: 'Deep nap', url: 'hmBBZUd-eDk'}, 
+    {title: 'Coming to you', url: 'OVR5SO0DvxE'}, {title: 'Day after day', url: '15wrFG1Gz84'}, {title: 'In my dreams', url: 'dYUiGU3v8ew'}, 
+    {title: 'How are you', url: 'J37gHwn9904'}, {title: 'Hope', url: '-cFpgpv4G0Y'}, {title: 'Crossroads', url: '38opfjfWlgY'}, 
+    {title: 'Return', url: 'aWDpwkKktaU'}, {title: 'Name of life', url: 'A1leiL8wknU'}, {title: 'When the morning comes', url: 'yhewMcvCyTU'}, 
+    {title: 'Flowing flower petal', url: 'RjzSPIdPXl0'}, {title: 'Spring night', url: '9aZmlntPYk4'}, {title: 'Cherry blossom dance', url: '9MPjcB4n-Vc'}, 
+    {title: 'Magnolia', url: '1AFW54NnDtQ'}, {title: 'Spring in winter', url: 'FHdmgF7h8Wk'}, {title: 'Snow and stars', url: 'GLxYjLkR1ts'}, 
+    {title: 'Mini bell', url: '5zsYEfujI24'}, {title: 'Deep blue of dawn', url: 'P0q29xLexXc'}, {title: 'Patting', url: 'pxCDtROfuFs'}, 
+    {title: 'A moment with you', url: '8HZ698TcLtU'}, {title: 'Small room and old piano', url: 'LlRX9FdUonI'}, {title: 'Whispering', url: '8BirxRSxOHk'}, 
+    {title: 'Hug in the moonlight', url: 'xzCw7lM4Cls'}, {title: 'Approach', url: 'TiURA4Ch1oU'}, {title: 'May 26, Memories', url: 'DSI6GXpkru0'}, 
+    {title: 'May 29, Love in me', url: '110JyIgQvOI'}, {title: 'Moon', url: 'Anyo9iUBjRk'}, {title: 'Wind', url: 'Y7BS3wZTVvM'}, 
+    {title: 'A letter from the moon', url: 'fxAYwvRjrjs'}, {title: 'Firefly', url: '6d65eRnhrAw'}, {title: 'Violet', url: 'AKfU9hnhck4'}, 
+    {title: 'Death on life', url: 'Dqf36LmjQHw'}, {title: 'Snow Blossom', url: 'SABakcXwFik'}, {title: 'The deep blue night', url: 'nZlfIGiwPZ8'}, 
+    {title: 'Silhouette of sunlight', url: 'mt9VSxZ1PKY'}, {title: 'Evening primrose', url: 'lHXJRx9Dq4Y'}, {title: 'Butterfly dance', url: 'SWQlsfE6OpQ'}, 
+    {title: 'Silent tremor', url: '0cwmFM0dEgw'}, {title: 'Wavy', url: 'QB-sCoQkhfQ'}, {title: 'Winter blue', url: 'FmZsX1f1esc'} ]
 
 const videoNum = videoUrlList.length;
 let currentVideoNum = 0;
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-        videoId: 'yRTnfhynaRc',
+        videoId: videoUrlList[0].url,
         events: {
             'onStateChange': onPlayerStateChange    // 플레이어 상태 변화 시 이벤트를 실행한다.
         }
     });
+    $title.textContent = videoUrlList[0].title + " by bluna";
 }
 
 function onPlayerStateChange(event) {
@@ -70,7 +64,7 @@ function onPlayerStateChange(event) {
         }
 
         player.loadVideoById(videoUrlList[currentVideoNum]);
-        $title.textContent = videoNameList[currentVideoNum] + " by 청월령";
+        $title.textContent = videoNameList[currentVideoNum] + " by bluna";
     }
 }
 
@@ -188,10 +182,28 @@ const reset = () => {
     if(validate()) {
         pause();
         setRemainTime();
+        shuffle(videoUrlList);
     }
 }
 
+function shuffle(array) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+}  
+
 changeVideo();
+shuffle(videoUrlList);
 $play.addEventListener("click", play);
 $pause.addEventListener("click", pause);
 $hourInput.addEventListener("change", reset);
