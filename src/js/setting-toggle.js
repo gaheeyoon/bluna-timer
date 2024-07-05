d33e00cd4e566be732d64e64e7f2d2b056347cdd
gaheeyoon <a5312559@gmail.com>
2024-07-05 22:09:45 +0900

    소개글 추가

--- src/js/setting-toggle.js
+++ src/js/setting-toggle.js
@@ -1,21 +1,41 @@
 const $settingBar = document.getElementById("setting-bar");
+const $infoBar = document.getElementById("info-bar");
 const $settingBtn = document.getElementById("setting-btn");
-const $closeBtn = document.getElementById("close-btn");
+const $infoBtn = document.getElementById("info-btn");
+const $settingCloseBtn = document.getElementById("setting-close-btn");
+const $infoCloseBtn = document.getElementById("info-close-btn");
 
 let isSettingBarOpen = false;
+let isInfoBarOpen = false;
+
 
 const settingBarToggle = () => {  
     if (isSettingBarOpen) {
         $settingBar.style.display = "none";
-        $settingBtn.style.display = "inline";
-        $closeBtn.style.display = "none";
+        $settingBtn.style.display = "inline-block";
+        $settingCloseBtn.style.display = "none";
     } else {
         $settingBar.style.display = "block";
         $settingBtn.style.display = "none";
-        $closeBtn.style.display = "inline";
+        $settingCloseBtn.style.display = "inline-block";
     }
     isSettingBarOpen = !isSettingBarOpen;
 };
 
+const infoBarToggle = () => {  
+    if (isInfoBarOpen) {
+        $infoBar.style.display = "none";
+        $infoBtn.style.display = "inline-block";
+        $infoCloseBtn.style.display = "none";
+    } else {
+        $infoBar.style.display = "block";
+        $infoBtn.style.display = "none";
+        $infoCloseBtn.style.display = "inline-block";
+    }
+    isInfoBarOpen = !isInfoBarOpen;
+};
+
 $settingBtn.addEventListener("click", settingBarToggle);
-$closeBtn.addEventListener("click", settingBarToggle);
+$infoBtn.addEventListener("click", infoBarToggle);
+$settingCloseBtn.addEventListener("click", settingBarToggle);
+$infoCloseBtn.addEventListener("click", infoBarToggle);
